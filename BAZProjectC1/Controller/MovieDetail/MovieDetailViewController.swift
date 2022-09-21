1fd0f1aa4c45c91a492467d3d1e8acb7249f7b54
Moises Abraham Vazquez Perez <mabraham.vazquez@gmail.com>
2022-09-21 09:49:37 -0500

    [ADD] Addition del controlador de detalle * Adición de la pantalla de detalle

---
+++ BAZProjectC1/Controller/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,14 @@
+//  MovieDetailViewController.swift
+//  BAZProjectC1
+//  Created by 291732 on 21/09/22.
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
