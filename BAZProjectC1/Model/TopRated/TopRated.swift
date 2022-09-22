dd1a4163a222dcef5cccc2c5edd1d834779a7a59
Moises Abraham Vazquez Perez <mabraham.vazquez@gmail.com>
2022-09-21 23:42:07 -0500

    [ADD] Adición el modelo para TopRated

---
+++ BAZProjectC1/Model/TopRated/TopRated.swift
@@ -0,0 +1,39 @@
+//  TopRated.swift
+//  BAZProjectC1
+//  Created by 291732 on 21/09/22.
+
+import Foundation
+
+struct TopRated : Codable {
+    let adult : Bool?
+    let backdropPath : String?
+    let genreIds : [Int]?
+    let id : Int?
+    let originalLanguage : String?
+    let originalTitle : String?
+    let overview : String?
+    let popularity : Double?
+    let posterPath : String?
+    let releaseDate : String?
+    let title : String?
+    let video : Bool?
+    let voteAverage : Double?
+    let voteCount : Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult = "adult"
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id = "id"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview = "overview"
+        case popularity = "popularity"
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title = "title"
+        case video = "video"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
