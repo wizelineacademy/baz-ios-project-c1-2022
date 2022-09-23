89ca016c080051daf6cfed9dc30b7bc364728440
Moises Abraham Vazquez Perez <mabraham.vazquez@gmail.com>
2022-09-23 01:43:11 -0500

    [UPDATE] Actualización de pantalla de Detalle * Eliminación de Assets necesarios * Adicion de icono para App

--- BAZProjectC1/Controller/CustomTabBarController.swift
+++ BAZProjectC1/Controller/CustomTabBarController.swift
@@ -6,12 +6,14 @@ import UIKit
 
 class CustomTabBarController: UITabBarController {
     
+    let wzlnRed: UIColor = UIColor(red: 117/255, green: 31/255, blue: 34/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 117/255, green: 31/255, blue: 34/255, alpha: 1)
-        UITabBar.appearance().barTintColor = .white
+        view.backgroundColor = wzlnRed
+        UITabBar.appearance().barTintColor = wzlnRed
+        tabBar.backgroundColor = wzlnRed
         tabBar.tintColor = .white
-        tabBar.backgroundColor = UIColor(red: 117/255, green: 31/255, blue: 34/255, alpha: 1)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,18 +46,10 @@ class CustomTabBarController: UITabBarController {
         let navController = UINavigationController(rootViewController: rootVC )
         navController.tabBarItem.title = title
         navController.tabBarItem.image = image
-        navController.navigationBar.prefersLargeTitles=true
+        navController.navigationBar.prefersLargeTitles = true
+        navController.navigationBar.titleTextAttributes = [.foregroundColor: wzlnRed]
+//        navController.navigationBar.backgroundColor = wzlnRed
         rootVC.navigationItem.title = title
         return navController
     }
-    
 }
-
-extension CustomTabBarController: UITabBarControllerDelegate {
-    
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        print("Selected \(viewController.title!)")
-    }
-    
-}
-
--- BAZProjectC1/Controller/MovieDetail/MovieDetailViewController.swift
+++ BAZProjectC1/Controller/MovieDetail/MovieDetailViewController.swift
@@ -8,7 +8,10 @@ final class MovieDetailViewController: UIViewController {
     //MARK: - O U T L E T S
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var imgMovie: UIImageView!
-    
+    @IBOutlet weak var lblOverview: UILabel!
+    @IBOutlet weak var lblVote: UILabel!
+    @IBOutlet weak var lblYear: UILabel!
+
     var index: Int?
     var objMovie: MovieAPIResponse?
     var objNowPlay: NowPlayingAPIResponse?
@@ -17,10 +20,11 @@ final class MovieDetailViewController: UIViewController {
         super.viewDidLoad()
         navigationController?.navigationBar.tintColor = .white
         self.setUpLeftMenu()
+        self.setInfoTrending()
     }
     
     private func setUpLeftMenu() {
-        let menuBarButton = UIBarButtonItem(image: UIImage(named:"back"), style: .plain, target: self, action:  #selector(popView))
+        let menuBarButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action:  #selector(popView))
         navigationItem.leftBarButtonItem = menuBarButton
     }
     
@@ -29,18 +33,16 @@ final class MovieDetailViewController: UIViewController {
     }
 
     private func setInfoTrending(){
+        let dVote: Double = objMovie?.movies?[index ?? 0].voteAverage ?? 0.0
         self.lblTitle.text = objMovie?.movies?[index ?? 0].title
+        self.lblOverview.text = objMovie?.movies?[index ?? 0].overview
+        self.lblVote.text = String(format: "%.1f", dVote )
+        self.lblYear.text = objMovie?.movies?[index ?? 0].releaseDate
         if let urlPoster = objMovie?.movies?[index ?? 0].backdropPath,
            let url = URL(string: "https://image.tmdb.org/t/p/w500\(urlPoster)"){
             let _ = imgMovie.loadImage(url: url)
+            
         }
-    }
     
-    private func setInfoNowPlaying() {
-        self.lblTitle.text = objNowPlay?.nowPlaying?[index ?? 0].title
-        if let urlPoster = objNowPlay?.nowPlaying?[index ?? 0].backdropPath,
-           let url = URL(string: "https://image.tmdb.org/t/p/w500\(urlPoster)"){
-            let _ = imgMovie.loadImage(url: url)
-        }
     }
 }
