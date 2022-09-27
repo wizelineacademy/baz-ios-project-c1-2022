674f2a72ecf17ecd69424c7c38c696aeb5be5a36
Carlos Alexis Cedeno Martinez <carlos.cedeno@elektra.com.mx>
2022-09-27 12:05:58 -0500

    Refactor code

--- BAZProjectC1/TrendingViewController.swift
+++ BAZProjectC1/TrendingViewController.swift
@@ -16,12 +16,8 @@ final internal class TrendingViewController: UICollectionViewController, Trendin
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
-//        collectionView.register(MovieMainListCollectionViewCell.self, forCellWithReuseIdentifier: MovieMainListCollectionViewCell.identifier)
         collectionView.register(UINib(nibName: "MovieMainListCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: MovieMainListCollectionViewCell.identifier)
         fetchMovies()
-//        let width = (view.frame.width-20)/3
-//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//        layout.itemSize = CGSize(width: width, height: width)
     }
     
     func fetchMovies() {
@@ -39,48 +35,7 @@ final internal class TrendingViewController: UICollectionViewController, Trendin
         imageArray.append(uiImage)
         return uiImage
     }
-
-    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 25
-//    }
-    
 }
-
-// MARK: - TableView's UICollectionViewDataSource
-/*
-extension TrendingViewController {
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        movies.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "TrendingTableViewCell")!
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let movieDetailVC = MovieDetailViewController(nibName: "MovieDetailViewController", bundle: .main)
-        movieDetailVC.movie = movies[indexPath.row]
-        movieDetailVC.img = imageArray[indexPath.row]
-        movieDetailVC.modalPresentationStyle = .fullScreen
-        self.present(movieDetailVC, animated: true, completion: nil)
-    }
-
-}
-
-// MARK: - TableView's UICollectionViewDataSource
-extension TrendingViewController {
-
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        var config = UIListContentConfiguration.cell()
-        config.text = movies[indexPath.row].title
-        config.image = retreiveImageFromSource(posterPath: movies[indexPath.row].posterPath)
-        cell.contentConfiguration = config
-    }
-
-}
-*/
 
 // MARK: - TrendingViewController's DataSource and Delegate
 extension TrendingViewController {
@@ -88,31 +43,11 @@ extension TrendingViewController {
         return movies.count
     }
 
-//    func collectionView(_ collectionView: UICollectionView, numberOfSections section: Int) -> Int {
-//        return 2
-//    }
-
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-//    {
-//            // In this function is the code you must implement to your code project if you want to change size of Collection view
-//            let width  = (view.frame.width-20)/3
-//            return CGSize(width: 0, height: 0)
-//    }
-//    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        print("sizeForItemAt")
-        return CGSize(width: 250, height: 200)
-    }
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieMainListCollectionViewCell", for: indexPath) as? MovieMainListCollectionViewCell else { return UICollectionViewCell() }
-        
-//        collectionView.
-        
         cell.setLabel(text: "\(movies[indexPath.row].title)")
         let image = retreiveImageFromSource(posterPath: movies[indexPath.row].posterPath)
         cell.setImage(img: image)
-//        cell.lblName.text = "\(indexPath.row)"
         return cell
     }
     
