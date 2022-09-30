92cd6aa39c8e3a79ad7d0a5c7ecb19be23a887e5
rnunezi <rocio.nunez@bancoazteca.com>
2022-09-30 13:01:07 -0500

    se atienden comentarios de PR

--- BAZProjectC1/Controller/DetailMoviesViewController.swift
+++ BAZProjectC1/Controller/DetailMoviesViewController.swift
@@ -8,28 +8,16 @@
 import UIKit
 
 class DetailMoviesViewController: UIViewController {
+    
     @IBOutlet weak var lblTitle: UILabel!
-    
     @IBOutlet weak var lblDescription: UILabel!
-    
     @IBOutlet weak var imgPoster: UIImageView!
     var strTitle:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         lblTitle.text = strTitle
-
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
